2a2d4d9873909f5576f73a71ef29331c1091e017
Marcus Escobedo <marcus14530@gmail.com>
2022-02-02 00:01:08 -0800

    Added data fetching from Beacon Chain

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-var path = require('path')
-const https = require('https')
+const validator = require('./Modules/validator');
+const https = require('https');
 const fs = require('fs');
 var Web3 = require('web3');
-var Eth = require('web3-eth');
 const ethers = require('ethers');
-const { parseBytes32String } = require('@ethersproject/strings');
-// "Eth.providers.givenProvider" will be set if in an Ethereum supported browser.
-var eth = new Eth(Eth.givenProvider || 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161');
-// or using the web3 umbrella package
 var web3 = new Web3(Web3.givenProvider || 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'); // -> web3.eth
 const utils = web3.utils;
 var BN = utils.BN;
@@ -20,12 +15,14 @@ const ABI = fs.readFileSync(__dirname + '\\JSON\\ETH2_Deposit_ABI.JSON'); // Con
 const ABI_JSON = JSON.parse(ABI);
 const inter = new ethers.utils.Interface(ABI_JSON);
 
+// API settings
 var ETHERSCAN_API_KEY = "";
-var INFURA_ETH2_API_KEY = "";
+var INFURA_PROJECT_SECRET = "";
+var INFURA_PROJECT_ID = "";
 const ETHERSCAN_SITE = "api.etherscan.io";
-const ETHERSCAN_ENDPOINT = "/api"
+const ETHERSCAN_ENDPOINT = "/api";
+const ETH2_API_SITE = "eth2-beacon-mainnet.infura.io";
 var TRANSACTIONS = "";
-var DEPOSITS = "";
 
 const readline = require('readline').createInterface({
     input: process.stdin,
@@ -34,15 +31,17 @@ const readline = require('readline').createInterface({
 
 
 function loadConfig() {
-    const data = fs.readFileSync('Config/config.ini', 'utf8');
+    var data = fs.readFileSync('Config/config.ini', 'utf8');
     var lines = data.split('\n');
     for (var i = 0; i < lines.length; i++) {
         var values = lines[i].split(':');
         if (values.length > 1) {
             var keyName = removeQuotes(values[0]);
             var value = removeQuotes(values[1]);
+            value = value.replace("\r", "");
             if (keyName == "ETHERSCAN_API_KEY") { ETHERSCAN_API_KEY = value; }
-            else if (keyName == "INFURA_ETH2_API_KEY") { INFURA_ETH2_API_KEY = value; }
+            else if (keyName == "INFURA_PROJECT_ID") { INFURA_PROJECT_ID = value; }
+            else if (keyName == "INFURA_PROJECT_SECRET") { INFURA_PROJECT_SECRET = value; }
         }
     }
 
@@ -64,14 +63,13 @@ function getTransactions() {
         "&offset=10000" +
         "&sort=asc" +
         `&apikey=${ETHERSCAN_API_KEY}`;
-
-    const options = {
+    var options = {
         hostname: ETHERSCAN_SITE,
         path: ETHERSCAN_ENDPOINT + txquery,
         port: 443,
         method: 'GET',
     }
-    const req = https.request(options, res => {
+    var req = https.request(options, res => {
         var data = '';
         res.on('data', d => {
             data += d.toString("ascii");
@@ -82,14 +80,46 @@ function getTransactions() {
             checkETH2Deposit();
         })
     });
-
     req.on('error', error => {
         console.error(error)
     })
-
     req.end()
-
 }
+
+function getValidatorInfo(pubKeys, ETH2_VALIDATORS) {
+    var query = "";
+    for (var i = 0; i < pubKeys.length; i++) {
+        if (i == 0) { query = '?'; }
+        else { query += '&'; }
+        query += `id=${pubKeys[i]}`;
+    }
+    var apiPath = `/eth/v1/beacon/states/head/validators${query}`;
+    var auth = `${INFURA_PROJECT_ID}:${INFURA_PROJECT_SECRET}`;
+    var encodedAuth = Buffer.from(auth).toString('base64');
+    var options = {
+        hostname: ETH2_API_SITE,
+        path: apiPath,
+        port: 443,
+        method: 'GET',
+        headers: {
+            'authorization': `Basic ${encodedAuth}`
+        }
+    }
+
+    https.get(options, res => {
+        var data = '';
+        res.on('data', d => {
+            data += d.toString("ascii");
+        });
+        res.on('end', () => {
+            var json = JSON.parse(data);
+            processDepositData(ETH2_VALIDATORS, json);
+        });
+    }).on('error', error => {
+        console.error(error)
+    });
+}
+
 function removeQuotes(text) {
     var txt = text;
     while (txt.includes("\"")) {
@@ -100,8 +130,8 @@ function removeQuotes(text) {
 
 function checkETH2Deposit() {
     var depoCount = 0; // The amount of deposits left to iterate through
-    var ETH2_VALIDATORS = "["; // Start JSON File
-    DEPOSITS = "["; // Start JSON File
+    var ETH2_VALIDATORS = new Array(); // ETH2 Validator array
+    var pubKeys = new Array(); // Validator pubkeys array
     for (let transaction of TRANSACTIONS) {
         var addy = transaction["to"]
         if (addy.toLowerCase() == ETH2_DEPO_ADDR.toLowerCase()) { // You can only send deposit transactions
@@ -109,70 +139,80 @@ function checkETH2Deposit() {
             var txnhash = transaction["hash"]
             depoCount++; // Increment the deposit count
             web3.eth.getTransactionReceipt(txnhash).then((value) => {
-                DEPOSITS += JSON.stringify(value) + ","; // Add deposit to list
-
-                // Can either wait for promises to complete and
-                // use the DEPOSITS/ETH2_VALIDATORS lists or
-                // do it live...
+                // Get data for validator array
                 var timestamp = transaction["timeStamp"];
                 var log = value["logs"][0];
                 var topics = log["topics"];
                 var data = log["data"];
                 var hash = log["transactionHash"];
-                var date = convertTicksToDate(timestamp);
+                //var date = convertTicksToDate(timestamp);
 
                 // Decode log data with contract ABI
-                const decodedInput = inter.parseLog({ data, topics }); // LogDescription variable
-                const args = decodedInput["args"]
+                var decodedInput = inter.parseLog({ data, topics }); // LogDescription variable
+                var args = decodedInput["args"]
 
                 var pubKey = args["pubkey"]; // Validator pubkey
                 var amtRaw = args["amount"]; // Amount staked in ETH
                 var indRaw = args["index"];  // Validator index number
+                var currentBalance = 0; // Balance of the validator
+                var rewardBalace = 0; // Income from staking ETH2
+                var effectiveBalance = 0; // Amount of ETH staking
+                var status = ""; // Status of the validator
+                var eligibleEpoch = ""; // Epoch when validator becomes eligible to activate
+                var activatedEpoch = ""; // Epoch when validator gets activated
+                var exitEpoch = ""; // Epoch when validator becomes eligible to exit
+                var withdrawalEpoch = ""; // Epoch when validator becomes eligible to withdraw
+                var slashed = ""; // Validator penalized for harmful behavior
                 var amtBytes = Buffer.from(utils.hexToBytes(amtRaw)); // Convert the hex data to bytes
                 var indBytes = Buffer.from(utils.hexToBytes(indRaw)); // Convert the hex data to bytes
                 var amt = utils.fromWei(new BN(amtBytes.readBigUInt64LE()), "gwei"); // Amount of ETH staked
                 var ind = indBytes.readBigInt64LE(); // Index of ETH2 validator - showing incorrect values to BeaconScan/Etherscan
-                var valURL = `https://beaconscan.com/validator/${pubKey}`; // URL for the validator info
-                var txURL = `https://etherscan.io/tx/${hash}`; // URL for the transaction info
-
-                // Print validator information
-                console.log(`${depoCount})`);
-                console.log(`Date: ${date}`);
-                //console.log(`Validator Index: ${ind}`);
-                console.log(`Validator Pubkey: ${pubKey}`);
-                //console.log(`Amount Deposited: ${amt} ETH`);
-                console.log(`Validator URL: ${valURL}`);
-                console.log(`Deposit Transaction: ${hash}`);
-                console.log(`Transaction URL: ${txURL}\n`);
-                var amount = amt.toString();
+                effectiveBalance = amt.toString();
                 var index = ind.toString();
+                pubKeys.push(pubKey);
 
-                // Create validator list for future uses
-                var validator = { timestamp, index, pubKey, amount, hash }; // Deposit transaction variable
-                ETH2_VALIDATORS += JSON.stringify(validator) + ",";
+                // Add validator to list 
+                var val = new validator(
+                    timestamp, index, pubKey, hash, effectiveBalance, currentBalance, rewardBalace,
+                    status, eligibleEpoch, activatedEpoch, exitEpoch, withdrawalEpoch, slashed
+                );
+                ETH2_VALIDATORS.push(val);
                 depoCount--;
                 if (depoCount <= 0) { // Once finished with all deposit transactions
-                    DEPOSITS = DEPOSITS.substring(0, DEPOSITS.length - 1); // Remove remaining commma
-                    DEPOSITS += "]"; // Close JSON
-                    ETH2_VALIDATORS = ETH2_VALIDATORS.substring(0, ETH2_VALIDATORS.length - 1); // Remove remaining commma
-                    ETH2_VALIDATORS += "]"; // Close JSON
-
-                    // Ask if want to search again...
+                    // Call function to process deposits and validators
+                    getValidatorInfo(pubKeys, ETH2_VALIDATORS);
                 }
             });
         }
     }
-    if (depoCount > 0) console.log(`ETH2 Validators\n--------------------------------------------------\n`);
+    if (depoCount > 0) console.log(`${depoCount} ETH2 Validators\n--------------------------------------------------\n`);
     else console.log("No ETH2 Validators linked to this wallet address!");
     // If user has more than 10k transactions
     if (TRANSACTIONS.length == 10000) { page++; getTransactions(); }
     else page = 1;
 }
 
-function convertTicksToDate(ticks) {
-    var dStart = new Date(1970, 0, 1);
-    dStart.setSeconds(ticks);
-    return dStart.toLocaleString();
+function processDepositData(ETH2_VALIDATORS, json) {
+    var valData = json["data"];
+    var validated = new Array();
+    for (let val of ETH2_VALIDATORS) {
+        for (var i = 0; i < valData.length; i++) {
+            var v = valData[i];
+            var vi = v["validator"];
+            var pubkey = vi["pubkey"];
+            if (validated.includes(pubkey)) continue;
+            if (pubkey == val.pubkey) {
+                // Update validator info with ETH2 data
+                val.updateInfo(v["index"], utils.fromWei(vi["effective_balance"], "gwei"), utils.fromWei(v["balance"], "gwei"), v["status"],
+                    vi["activation_eligibility_epoch"], vi["activation_epoch"], vi["exit_epoch"], vi["withdrawal_epoch"], vi["slashed"]);
+                val.print();
+                console.log("");
+                validated.push(pubkey);
+                break;
+            }
+            if (i == valData.length - 1) console.log(`${val.pubkey} not validated!`); // Debugging
+        }
+    }
 }
 
 loadConfig();
